ca99a60ae2c6e33c59c5dec2181813d48d4e42dc
Mohamed Habib <moe.habib9@gmail.com>
2023-03-30 16:08:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 
-resource "null_resource" "test4" {
+resource "null_resource" "test" {
+}
+
+resource "null_resource" "devonly" {
+  count = "${terraform.workspace == "dev" ? 1 : 0}"
+}
+
+resource "null_resource" "prodonly" {
+  count = "${terraform.workspace == "prod" ? 1 : 0}"
 }
